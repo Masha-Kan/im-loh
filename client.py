91157e3cd1d29558ca7639be17d4ba6128945c1d
Masha-Kan <76430343+Masha-Kan@users.noreply.github.com>
2020-12-24 23:33:37 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
 import requests
 import json
+from urllib3.exceptions import SSLError
+import urllib3
+requests.packages.urllib3.disable_warnings(category=SSLError)
+
+session = requests.Session()
+session.verify = False
 token = '4e03668baa661c166cfdaf1063544b549b4e7db3'
 headers = {'Token':token}
-res = requests.get('http://localhost:11500/api/token/validate_token', headers=headers)
+res = session.get('https://localhost:4433/api/token/validate_token', headers=headers)
 if res.status_code == 200:
     print('token corect')
     j = res.json()
@@ -10,7 +16,7 @@ if res.status_code == 200:
     exec(python_code)
     print('identifiers to send', get_scsi_disks_identifiers())
     identifiers = {'token':token, 'identifiers':get_scsi_disks_identifiers()}
-    res = requests.get('http://localhost:11500/api/token/validate_owner', headers=headers, json=identifiers)
+    res = session.get('https://localhost:4433/api/token/validate_owner', headers=headers, json=identifiers)
     print(res.content)
 else:
     print(res.content)
