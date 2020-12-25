5b52f784de7a8d3804d023fe0b3593a2ccb95069
huhguz <huhguz@mail.ru>
2020-12-25 23:06:54 +0300

    wtf

--- test_service.py
+++ test_service.py
@@ -2,5 +2,17 @@ import pytest
 
 
 
-def test_token_validate__code_returned():
-    
+def test_token_validate___owner_validate_code_returned():
+    assert True
+
+
+def test_owner_validate__cheat_code_returned():
+    assert True
+
+
+def test_token_validate__error_returned():
+    assert True
+
+
+def test_owner_validate__error_returned():
+    pass
